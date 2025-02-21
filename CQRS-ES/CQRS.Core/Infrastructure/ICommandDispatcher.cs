d996f3206899b2340664e81216b250e5fe031570
David Lam <David.Lam@majesco.com>
2025-02-20 17:31:58 -1000

    Completed Module 4 - CommandDispatching

---
+++ CQRS-ES/CQRS.Core/Infrastructure/ICommandDispatcher.cs
@@ -0,0 +1,15 @@
+﻿using CQRS.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Core.Infrastructure
+{
+    public interface ICommandDispatcher
+    {
+        void Registerhandler<T>(Func<T, Task> handler) where T : BaseCommand;
+        Task SendAsync(BaseCommand command);
+    }
+}
--- SM-Post/Post.Query/Post.Query.Infrastructure/obj/Debug/net9.0/Post.Query.Infrastructure.AssemblyInfo.cs
+++ SM-Post/Post.Query/Post.Query.Infrastructure/obj/Debug/net9.0/Post.Query.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Post.Query.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88e425914115a6571b2a09fd0345e4f273587a18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d01ee919c2b5b394e8b7c6669d763cc7771030b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Post.Query.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Post.Query.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
